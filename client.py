d85c92fd9f4407ae32d1c3b14e9fd367cdf8bb5d
Mark Fickett <mark.fickett@gmail.com>
2015-01-05 22:12:22 -0500

    Register players, send moves, get game state with player positions.

--- client.py
+++ client.py
@@ -1,18 +1,37 @@
 # http://pythonhosted.org/Pyro4/intro.html#simple-example
 
+import random
+import time
+
 import Pyro4
 
+import common
 import messages_pb2
-import server
 
 
 if __name__ == '__main__':
-  server.RegisterProtoSerialization()
+  secret = str(random.random())
+  common.RegisterProtoSerialization()
+  s = Pyro4.Proxy('PYRONAME:%s' % common.SERVER_URI_NAME)
 
   name = raw_input('What is your name? ').strip()
 
-  greeting_maker = Pyro4.Proxy('PYRONAME:%s' % server.SERVER_URI_NAME)
-  response = greeting_maker.get_fortune(messages_pb2.Request(name=name))
-  print (
-      '%s Your lucky number is %d.'
-      % (response.fortune, response.lucky_number))
+  s.Register(messages_pb2.RegisterRequest(
+      player_secret=secret, player_name=name))
+
+  try:
+    while True:
+      time.sleep(0.1)
+      if random.random() > 0.9:
+        move = messages_pb2.Coordinate(
+            x=random.randint(-1, 1),
+            y=random.randint(-1, 1))
+        s.Move(messages_pb2.MoveRequest(
+            player_secret=secret,
+            move=move))
+      state = s.GetGameState()
+      for player in state.player:
+        print '%s:\t%d\t%d\t' % (player.name, player.pos.x, player.pos.y),
+      print ''
+  except KeyboardInterrupt:
+    pass
