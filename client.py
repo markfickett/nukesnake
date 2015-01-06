9cefaa16e89578957070df2ab2ed34b1c8ef0565
Mark Fickett <mark.fickett@gmail.com>
2015-01-05 20:26:45 -0500

    Add Pyro4 simple client/server example.

---
+++ client.py
@@ -0,0 +1,8 @@
+# http://pythonhosted.org/Pyro4/intro.html#simple-example
+
+import Pyro4
+
+name=raw_input("What is your name? ").strip()
+
+greeting_maker=Pyro4.Proxy("PYRONAME:example.greeting")    # use name server object lookup uri shortcut
+print greeting_maker.get_fortune(name)
