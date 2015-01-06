9edd1b4b8c463dbf640234b5b119f4120bc6adf3
Mark Fickett <mark.fickett@gmail.com>
2015-01-05 21:17:30 -0500

    Add proto file and custom (de)serialization of protos.

--- client.py
+++ client.py
@@ -2,7 +2,17 @@
 
 import Pyro4
 
-name=raw_input("What is your name? ").strip()
+import messages_pb2
+import server
 
-greeting_maker=Pyro4.Proxy("PYRONAME:example.greeting")    # use name server object lookup uri shortcut
-print greeting_maker.get_fortune(name)
+
+if __name__ == '__main__':
+  server.RegisterProtoSerialization()
+
+  name = raw_input('What is your name? ').strip()
+
+  greeting_maker = Pyro4.Proxy('PYRONAME:%s' % server.SERVER_URI_NAME)
+  response = greeting_maker.get_fortune(messages_pb2.Request(name=name))
+  print (
+      '%s Your lucky number is %d.'
+      % (response.fortune, response.lucky_number))
