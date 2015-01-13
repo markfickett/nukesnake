bf2a532725d95785043c43ab89a98364016cb783
Mark Fickett <mark.fickett@gmail.com>
2015-01-12 21:35:30 -0500

    Refactor client into class. Prepare for same-keyboard multiplayer.

--- client.py
+++ client.py
@@ -42,140 +42,191 @@ _PLAYER_COLORS = (
 )
 
 
-def _DrawingMain(window, player_id, player_name):
-  curses.curs_set(False)
-  window.nodelay(True)
-  last_state_hash = None
+_MOVE_KEYS = (
+    {
+       ord('q'): (-1, -1),
+       ord('w'): (0, -1),
+       ord('e'): (1, -1),
+       ord('a'): (-1, 0),
+       ord('s'): (0, 1),
+       ord('d'): (1, 0),
+       ord('z'): (-1, 1),
+       ord('x'): (0, 1),
+       ord('c'): (1, 1),
+    },
+)
+_MAX_LOCAL_PLAYERS = len(_MOVE_KEYS)
+_ACTION_KEYS = (
+    ord(' '),
+)
+assert _MAX_LOCAL_PLAYERS == len(_ACTION_KEYS)
 
-  block_palettes = {}
-  palette_id = 1
-  for fg, block_types in _BLOCK_FOREGROUNDS.iteritems():
-    for block_type in block_types:
-      block_palettes[block_type] = palette_id
-    curses.init_pair(palette_id, fg, _BG_COLOR)
-    palette_id += 1
-  player_palettes = []
-  for color in _PLAYER_COLORS:
-    curses.init_pair(palette_id, color, _BG_COLOR)
-    player_palettes.append(palette_id)
-    palette_id += 1
 
-  while True:
-    time.sleep(_UPDATE_INTERVAL)
+class Client:
+  def __init__(self, game_server):
+    self._game_server = game_server
+    self._players_secret_and_info = []
+    self._local_player_ids = set()
+    self._player_info_by_id = {}
 
-    move_key = window.getch()
-    curses.flushinp()
-    x, y = {
-        ord('q'): (-1, -1),
-        ord('w'): (0, -1),
-        ord('e'): (1, -1),
-        ord('a'): (-1, 0),
-        ord('s'): (0, 1),
-        ord('d'): (1, 0),
-        ord('z'): (-1, 1),
-        ord('x'): (0, 1),
-        ord('c'): (1, 1),
-    }.get(move_key, (0, 0))
+    self._window = None
+    self._block_palettes_by_type = {}
+    self._player_palettes = []
+
+    self._game_state = None
+
+  def Register(self, name):
+    secret = str(random.random())
+    resp = self._game_server.Register(messages_pb2.RegisterRequest(
+        player_secret=secret, player_name=name))
+    self._players_secret_and_info.append((secret, resp.player))
+    self._local_player_ids.add(resp.player.player_id)
+    self._player_info_by_id[resp.player.player_id] = resp.player
+
+  def UnregisterAll(self):
+    for secret, _ in self._players_secret_and_info:
+      self._game_server.Unregister(
+          messages_pb2.IdentifiedRequest(player_secret=secret))
+    self._players_secret_and_info = []
+    self._local_player_ids = set()
+    self._player_info_by_id = {}
+
+  @staticmethod
+  def CursesWrappedLoop(window, client):
+    client._CursesWrappedLoop(window)
+
+  def _CursesWrappedLoop(self, window):
+    self._SetUpCurses(window)
+
+    while True:
+      time.sleep(_UPDATE_INTERVAL)
+
+      key_code = window.getch()
+      curses.flushinp()
+      for i, (secret, info) in enumerate(self._players_secret_and_info):
+        self._DoPlayerCommand(i, secret, info, key_code)
+
+      if self._UpdateGameState():
+        self._Repaint()
+
+  def _SetUpCurses(self, window):
+    self._window = window
+    curses.curs_set(False)
+    self._window.nodelay(True)
+
+    palette_id = 1
+    for fg, block_types in _BLOCK_FOREGROUNDS.iteritems():
+      for block_type in block_types:
+        self._block_palettes_by_type[block_type] = palette_id
+      curses.init_pair(palette_id, fg, _BG_COLOR)
+      palette_id += 1
+    for color in _PLAYER_COLORS:
+      curses.init_pair(palette_id, color, _BG_COLOR)
+      self._player_palettes.append(palette_id)
+      palette_id += 1
+
+  def _DoPlayerCommand(self, i, secret, info, key_code):
+    x, y = _MOVE_KEYS[i].get(key_code, (0, 0))
     if x or y:
-      s.Move(messages_pb2.MoveRequest(
+      self._game_server.Move(messages_pb2.MoveRequest(
           player_secret=secret,
           move=messages_pb2.Coordinate(x=x, y=y)))
-    if move_key == ord(' '):
-      s.Action(messages_pb2.IdentifiedRequest(player_secret=secret))
+    if key_code == _ACTION_KEYS[i]:
+      self._game_server.Action(
+          messages_pb2.IdentifiedRequest(player_secret=secret))
 
+  def _UpdateGameState(self):
     state_req = messages_pb2.GetGameStateRequest()
-    if last_state_hash is not None:
-      state_req.hash = last_state_hash
-    state = s.GetGameState(state_req)
+    if self._game_state is not None:
+      state_req.hash = self._game_state.hash
+    state = self._game_server.GetGameState(state_req)
     if not state:
-      continue
-    last_state_hash = state.hash
+      return False
+    self._game_state = state
+    for info in self._game_state.player_info:
+      if info.player_id in self._player_info_by_id:
+        self._player_info_by_id[info.player_id].MergeFrom(info)
+    return True
 
-    h, w = window.getmaxyx()
-    if state.size.x >= w or state.size.y >= h:
-      window.erase()
-      _RenderMessage(
-          window,
+  def _Repaint(self):
+    h, w = self._window.getmaxyx()
+    if self._game_state.size.x >= w or self._game_state.size.y >= h:
+      self._window.erase()
+      self._RenderMessage(
           'Resize to %dx%d (now %dx%d).'
-          % (state.size.x + 1, state.size.y + 1, w, h))
-      window.refresh()
-      continue
+          % (self._game_state.size.x + 1, self._game_state.size.y + 1, w, h))
+      self._window.refresh()
+      return
 
-    self_info = None
-    living_info = None
-    for info in state.player_info:
-      if info.player_id == player_id:
-        self_info = info
-      if info.alive:
-        living_info = info
-
-    window.erase()
-    for block in state.block:
-      _RenderBlock(
-          block,
-          window,
-          player_id,
-          state.stage,
-          block_palettes,
-          player_palettes)
-    if state.stage == messages_pb2.GameState.COLLECT_PLAYERS:
-      _RenderMessage(window, 'Press space to start.')
-    elif state.stage == messages_pb2.GameState.ROUND_START:
-      _RenderMessage(window, 'Ready...')
-    elif not self_info.alive:
-      _RenderMessage(
-          window, '%s Dies (score %d)' % (self_info.name, self_info.score))
-    if state.stage == messages_pb2.GameState.ROUND_END and living_info:
-      _RenderMessage(
-          window,
-          '%s wins! (score %d)'
-          % (living_info.name, living_info.score), y_offset=1)
-    window.refresh()
+    self._window.erase()
+    for block in self._game_state.block:
+      self._RenderBlock(block)
+    if self._game_state.stage == messages_pb2.GameState.COLLECT_PLAYERS:
+      self._RenderMessage('Press action to start.')
+    elif self._game_state.stage == messages_pb2.GameState.ROUND_START:
+      self._RenderMessage('Ready...')
+    else:
+      for i, (_, local_info) in enumerate(self._players_secret_and_info):
+        if not local_info.alive:
+          self._RenderMessage(
+              '%s Dies (score %d)' % (local_info.name, local_info.score),
+              y_offset=-(i + 1))
+    if self._game_state.stage == messages_pb2.GameState.ROUND_END:
+      living_info = None
+      for info in self._game_state.player_info:
+        if info.alive:
+          living_info = info
+          break
+      if living_info:
+        self._RenderMessage(
+            '%s wins! (score %d)' % (living_info.name, living_info.score))
+    self._window.refresh()
 
 
-def _RenderMessage(window, msg, y_offset=0):
-  h, w = window.getmaxyx()
-  window.addstr(h / 2 + y_offset, w / 2 - len(msg) / 2, msg)
+  def _RenderBlock(self, block):
+    s = '?'
+    s_attr = curses.A_NORMAL
+    if block.type in (_B.PLAYER_HEAD, _B.PLAYER_TAIL):
+      palette_id = self._player_palettes[
+          block.player_id % len(self._player_palettes)]
+    else:
+      palette_id = self._block_palettes_by_type.get(block.type)
+    if palette_id:
+      s_attr += curses.color_pair(palette_id)
+    if block.type == _B.PLAYER_HEAD:
+      s = _PLAYER_ICONS[block.player_id % len(_PLAYER_ICONS)]
+      if (self._game_state.stage != messages_pb2.GameState.ROUND
+          and block.player_id in self._local_player_ids):
+        s_attr += curses.A_BLINK
+    else:
+      s = _BLOCK_CHARACTERS.get(block.type, _DEFAULT_BLOCK_CHARACTER)
+    self._window.addstr(block.pos.y, block.pos.x, s.encode('utf-8'), s_attr)
 
 
-def _RenderBlock(
-    block,
-    window,
-    player_id,
-    stage,
-    block_palettes,
-    player_palettes):
-  s = '?'
-  s_attr = curses.A_NORMAL
-  if block.type in (_B.PLAYER_HEAD, _B.PLAYER_TAIL):
-    palette_id = player_palettes[block.player_id % len(player_palettes)]
-  else:
-    palette_id = block_palettes.get(block.type)
-  if palette_id:
-    s_attr += curses.color_pair(palette_id)
-  if block.type == _B.PLAYER_HEAD:
-    s = _PLAYER_ICONS[block.player_id % len(_PLAYER_ICONS)]
-    if stage != messages_pb2.GameState.ROUND and block.player_id == player_id:
-      s_attr += curses.A_BLINK
-  else:
-    s = _BLOCK_CHARACTERS.get(block.type, _DEFAULT_BLOCK_CHARACTER)
-  window.addstr(block.pos.y, block.pos.x, s.encode('utf-8'), s_attr)
+  def _RenderMessage(self, msg, y_offset=0):
+    h, w = self._window.getmaxyx()
+    self._window.addstr(h / 2 + y_offset, w / 2 - len(msg) / 2, msg)
+
+
+def Main():
+  common.RegisterProtoSerialization()
+  game_server = Pyro4.Proxy('PYRONAME:%s' % common.SERVER_URI_NAME)
+  locale.setlocale(locale.LC_ALL, '')
+
+  client = Client(game_server)
+  names = []
+  while len(names) < _MAX_LOCAL_PLAYERS:
+    name = raw_input('Name for player %d? ' % (len(names) + 1)).strip()
+    if not name:
+      break
+    names.append(name)
+    client.Register(name)
+
+  try:
+    curses.wrapper(Client.CursesWrappedLoop, client)
+  finally:
+    client.UnregisterAll()
 
 
 if __name__ == '__main__':
-  secret = str(random.random())
-  common.RegisterProtoSerialization()
-  s = Pyro4.Proxy('PYRONAME:%s' % common.SERVER_URI_NAME)
-
-  name = raw_input('What is your name? ').strip()
-
-  resp = s.Register(messages_pb2.RegisterRequest(
-      player_secret=secret, player_name=name))
-  player_id = resp.player.player_id
-
-  locale.setlocale(locale.LC_ALL, '')
-
-  try:
-    curses.wrapper(_DrawingMain, player_id, name)
-  finally:
-    s.Unregister(messages_pb2.IdentifiedRequest(player_secret=secret))
+  Main()
