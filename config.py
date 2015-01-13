91bf7cffe0222788691ee0fcdf72ea8a55a66dd3
Mark Fickett <mark.fickett@gmail.com>
2015-01-12 22:27:18 -0500

    Add mine and ammo rarity to config.

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ HEIGHT = 50
 STARTING_TAIL_LENGTH = 50
 LOCALHOST_ONLY = True
 INFINITE_AMMO = False
+AMMO_RARITY = 400  # 1 in this many blocks is ammo.
 MINES = True
+MINE_RARITY = 500
+
 
 # Client only.
 NO_NETWORK = True
