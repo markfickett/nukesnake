de14cae1faadef0b6120e9d2d1ba507c72f39aa7
Mark Fickett <mark.fickett@gmail.com>
2015-01-14 00:06:14 -0500

    Add terrain generator.

---
+++ height_map.py
@@ -0,0 +1,45 @@
+import random
+
+import messages_pb2
+
+
+def MakeHeightMap(size, min_value, max_value, blur_size=2):
+  rand_vals = []
+  smoothed = []
+  for _ in xrange(size.x):
+    row = []
+    for _ in xrange(size.y):
+      row.append(random.randint(min_value, max_value))
+    rand_vals.append(row)
+    smoothed.append([None] * size.y)
+  area = (2 * blur_size + 1) ** 2
+  for x in xrange(size.x):
+    for y in xrange(size.y):
+      v = 0
+      for i in xrange(-blur_size, blur_size + 1):
+        for j in xrange(-blur_size, blur_size + 1):
+          v += rand_vals[(x + i) % size.x][(y + j) % size.y]
+      smoothed[x][y] = v / area
+  return smoothed
+
+
+if __name__ == '__main__':
+  grid = MakeHeightMap(messages_pb2.Coordinate(x=20, y=70), 0, 20)
+  if False:
+    for row in grid:
+      for v in row:
+        print ('%2d' % v),
+      print ''
+  for row in grid:
+    row_str = []
+    for v in row:
+      if v >= 13:
+        c = u'\N{FULL BLOCK}'
+      elif v >= 12:
+        c = u'\N{MEDIUM SHADE}'
+      elif v >= 11:
+        c = u'\N{LIGHT SHADE}'
+      else:
+        c = ' '
+      row_str.append(c)
+    print ''.join(row_str)
