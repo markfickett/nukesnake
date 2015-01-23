fdc9a19a4e38bd961557c11eea953d06b0cda908
Mark Fickett <mark.fickett@gmail.com>
2015-01-22 19:35:25 -0500

    Make the AI player gain abilities over different rounds.

--- ai_player.py
+++ ai_player.py
@@ -1,13 +1,13 @@
 """Computer-controlled snake."""
 
 
+import ai_player_pb2
 import common
 import config
 import game_pb2
 import random
 
 
-_VIEW_DIST = 3
 _DESIRABLE_BLOCKS = frozenset((
     game_pb2.Block.AMMO,
     game_pb2.Block.STAY_STILL,
@@ -20,8 +20,30 @@ class Player(object):
     self._secret = secret
     self._info = player_info
 
+    self._SetAbilities(0)
+
+  def _SetAbilities(self, round_num):
+
+    if round_num >= 13:
+      self._view_dist = 3
+    elif round_num >= 10:
+      self._pick_desirable_blocks = True
+    elif round_num >= 7:
+      self._view_dist = 2
+      self._shooting = ai_player_pb2.Shoot.WHEN_CORNERED
+    elif round_num >= 5:
+      self._shooting = ai_player_pb2.Shoot.IMMEDIATE
+    elif round_num >= 3:
+      self._diagonals = True
+    else:
+      self._view_dist = 1
+      self._shooting = ai_player_pb2.Shoot.NEVER
+      self._diagonals = False
+      self._pick_desirable_blocks = False
+
   def UpdateAndDoCommands(self, new_game_state, game_server):
     if new_game_state.stage != game_pb2.Stage.ROUND:
+      self._SetAbilities(new_game_state.round_num)
       return
 
     for player_info in new_game_state.player_info:
@@ -45,9 +67,9 @@ class Player(object):
     default_dir = (player_head.direction.x, player_head.direction.y)
 
     nearby = []
-    for dx in xrange(-_VIEW_DIST, _VIEW_DIST + 1):
+    for dx in xrange(-self._view_dist, self._view_dist + 1):
       row = []
-      for dy in xrange(-_VIEW_DIST, _VIEW_DIST + 1):
+      for dy in xrange(-self._view_dist, self._view_dist + 1):
         row.append(grid[
             (player_head.pos.x + dx) % new_game_state.size.x][
             (player_head.pos.y + dy) % new_game_state.size.y])
@@ -60,16 +82,19 @@ class Player(object):
       for j in xrange(-1, 2):
         if i == 0 and j == 0:
           continue
+        if not self._diagonals and (i != 0 and j != 0):
+          continue
         clear_in_view = True
-        for dist in range(1, _VIEW_DIST + 1):
-          block = nearby[_VIEW_DIST + i * dist][_VIEW_DIST + j * dist]
+        for dist in range(1, self._view_dist + 1):
+          block = nearby[self._view_dist + i * dist][self._view_dist + j * dist]
           if block is None:
             if dist == 1:
               safe_directions.add((i, j))
           elif (block.type in _DESIRABLE_BLOCKS or
-              (block.type == game_pb2.Block.ROCKET and
-               block.direction.x == i and block.direction.y == j)):
-            preferred_directions.add((i, j))
+               (block.type == game_pb2.Block.ROCKET and
+                block.direction.x == i and block.direction.y == j)):
+            if self._pick_desirable_blocks:
+              preferred_directions.add((i, j))
           else:
             clear_in_view = False
             break
@@ -92,5 +117,9 @@ class Player(object):
       game_server.Move(
           self._secret, game_pb2.Coordinate(x=new_dir[0], y=new_dir[1]))
 
-    if (config.INFINITE_AMMO or self._info.inventory) and shoot:
+    if self._shooting == ai_player_pb2.Shoot.NEVER:
+      shoot = False
+    elif self._shooting == ai_player_pb2.Shoot.IMMEDIATE:
+      shoot = True
+    if shoot and (config.INFINITE_AMMO or self._info.inventory):
       game_server.Action(self._secret)
