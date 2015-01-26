02cb7afb1739ddbb4680fb285d2a752f54fc1b8f
Mark Fickett <mark.fickett@gmail.com>
2015-01-25 22:09:40 -0500

    Add config call for logging.

--- common.py
+++ common.py
@@ -1,5 +1,13 @@
+import logging
+
 def MakeGrid(size):
   grid = []
   for x in range(size.x):
     grid.append([None] * size.y)
   return grid
+
+
+def ConfigureLogging():
+  logging.basicConfig(
+      format='%(levelname)s %(asctime)s %(filename)s:%(lineno)s: %(message)s',
+      level=logging.INFO)
