c546452d5ba526c33080cc6588aae5c776dded2f
Mark Fickett <mark.fickett@gmail.com>
2015-02-07 00:13:20 -0500

    Add LD_LIBRARY_PATH update to common for use when packaged as a (mac) app.

--- common.py
+++ common.py
@@ -18,6 +18,13 @@ def ConfigureLogging(**kwargs):
 
 # Centralize proto imports which have special requirements.
 
+# For bundled app distribution, include the local directory in LD_LIBRARY_PATH.
+resources_dir = os.path.dirname(__file__)
+if '.app' in resources_dir:
+  os.chdir(resources_dir)
+  os.environ['LD_LIBRARY_PATH'] = resources_dir + ':' + os.environ.get(
+      'LD_LIBRARY_PATH', '')
+
 # A bottleneck for network communication is (de)serialization of protos when
 # the pure-Python implementation is used. Prefer the C++ bindings.
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'cpp'
